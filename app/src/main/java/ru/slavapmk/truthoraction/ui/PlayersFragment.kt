a7672e8da5b232aa93b750b0e796e32f511bcecb
Slava Petrov <slavapmk@gmail.com>
2024-11-11 14:55:43 +0300

    Reseting status on player update

--- app/src/main/java/ru/slavapmk/truthoraction/ui/PlayersFragment.kt
+++ app/src/main/java/ru/slavapmk/truthoraction/ui/PlayersFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.EditorInfo
 import androidx.core.content.edit
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -37,35 +38,53 @@ class PlayersFragment : Fragment() {
         binding.playersList.layoutManager = LinearLayoutManager(context)
 
         binding.adduser.setEndIconOnClickListener {
-            if (binding.adduserEdit.text?.isNotEmpty() == true) {
-                players.players.add(
-                    Player(binding.adduserEdit.text.toString())
-                )
-                binding.adduserEdit.text?.clear()
-                updatePlayers(players)
-                playersAdapter.notifyItemInserted(players.players.size)
+            addPeople()
+        }
+
+        binding.adduserEdit.setOnEditorActionListener { _, actionId, _ ->
+            return@setOnEditorActionListener if (actionId == EditorInfo.IME_ACTION_DONE) {
+                addPeople()
+                true
+            } else {
+                false
             }
         }
 
         return binding.root
     }
 
-    fun removePlayer(player: Player) {
+    private fun addPeople() {
+        if (binding.adduserEdit.text?.isNotEmpty() == true) {
+            players.players.add(
+                Player(binding.adduserEdit.text.toString())
+            )
+            binding.adduserEdit.text?.clear()
+            updatePlayers(players)
+            playersAdapter.notifyItemInserted(players.players.size)
+        }
+    }
+
+    private fun removePlayer(player: Player) {
         val index = players.players.indexOf(player)
         players.players.removeAt(index)
         updatePlayers(players)
         playersAdapter.notifyItemRemoved(index)
     }
 
-    fun getPlayers() = activity.playersCodec.decodePlayers(
+    private fun getPlayers() = activity.playersCodec.decodePlayers(
         activity.shared.getString(
             "players",
             activity.playersCodec.encodePlayers(Players())
         )!!
     )
 
-    fun updatePlayers(players: Players) = activity.shared.edit {
+    private fun updatePlayers(players: Players) = activity.shared.edit {
         players.current = 0
+        activity.shared.edit {
+            putString("answer_type", "none")
+            putString("current", null)
+            commit()
+        }
         putString(
             "players",
             activity.playersCodec.encodePlayers(players)
